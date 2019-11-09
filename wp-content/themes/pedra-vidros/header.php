216177876f1021acee2ebe71ed79868e69db7589
Tiqueliro <matteus.camargo@gmail.com>
2019-11-09 01:57:30 -0300

    a

--- wp-content/themes/pedra-vidros/header.php
+++ wp-content/themes/pedra-vidros/header.php
@@ -34,43 +34,47 @@
 <body <?php body_class(); ?>>
 <div id="wrap">
 <header>
+    <a href="index.php" >
+        <div  id="logo" class = "barraHeader">
+            <img src="wp-content\themes\pedra-vidros\images\logo-png.png" id ="imglogo">
+        </div>
+    </a>
     <a href="index.php">
-        <div  id="Inicio">
-            <img src="wp-content\themes\pedra-vidros\images\logo-png.png" id ="logo">
+        <div  id="Inicio" class = "barraHeader">
+           Inicio
         </div>
     </a>
-    <div class="dropdown textos2" id="Exames" >"LOREM"
-        <div class="dropdown-content textos2">
-            <ul id="menuexames" class="lista textos2">
-                            
-            </ul>
-        </div>
+    <a>
+    <div class = "barraHeader" id = "Sobre" >"Sobre"
+      
     </div>
-    <a href="agendar.php"><div class="dropdown textos2" id="Agendar">"LOREM"</div>
     </a>
-    <div class="dropdown textos2" id="Login"><a href="usuario.php">"LOREM"</a>
-            <div class="dropdown-content" id="divcontent2">
-                <ul class="lista textos2">
-                </ul>
-            </div>
+        <a href="agendar.php">
+        <div class = "barraHeader" id="Servicos">
+        Servicos
         </div>
-        <a href="login.php"><div class="dropdown textos2" id="Login">"LOREM"</div>
-    </a>
-    <a href="Cadastro.php"><div class="dropdown textos2" id="Cadastro">"LOREM"</div></a>
+        </a>
+        <a >
+    <div class = "barraHeader" id="Contato">
+        Contato
+     </div>
+     </a>
+    
 </header>
     <div class="content-wrapper">
+
+
+
+
       <style>
-            
-}
 div{
     width: 100%;
 }
-#logo{
-    height:80px;
+#imglogo{
     width:80px;
 }
 #Inicio{
-    width:20%;
+  width:20%;
   position: relative;
   float:left;
   padding-top: 20px;
@@ -78,27 +82,12 @@ div{
   background-color: #07dfb4;
   text-align:center;
 }
-.textos{
-    font-family:tahoma;
-    font-size: 25px;
-}
-h{
- font-family:tahoma;   
-}
-
 
 a{
     color: inherit;
     text-decoration: none;
 }
-#texto1{
-/*    border: 1px solid black;*/
-    margin-top: -200px;
-    text-align:center;
-    width: 100%;
-    
-}
-.dropdown {
+.barraHeader {
   position: relative;
   display: inline-block;
   float:left;
@@ -109,96 +98,6 @@ a{
   text-align:center;
   color: white;
 }
-
-#Inicio:hover{
-    animation-name: hover;
-     animation-duration: 0.3s;
-     background-color:#07d1b4;
-}
-
-
-
-.dropdown:hover{
-     animation-name: hover;
-     animation-duration: 0.3s;
-     background-color:#07d1b4;
-}
-.dropdown-content{
-  position: absolute;
-  background-color: #4CFFDBaa;
-  min-width: 160px;
-  z-index: 1;
-  width: 100%;
-  margin-top:20px;
-  height: 0px;
-  transition-property: height, display;
-  transition-duration: 0.3s;
-  -webkit-transition:  height 0.3s;
-  transition-timing-function: ease;
-}
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-.dropdown-content a:hover {
-    color:white;
-    background-color: #07dfb4;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-  margin-top: 20px;
-  height: 450px;
-}
-
-@keyframes hover{
-  from{ background-color: #07dfb4;} 
-  to{ background-color: #07d1b4;}
-}
-
-@keyframes lista{
-    from{display: none;}
-    to{display: block;}
-}
-
-
-.lista {
-   display: block;
-   height: 0%;
-   transition-property: height, display;
-  transition-duration: 1s;
-  -webkit-transition:  height 1s, display 1s;
-  transition-timing-function: linear;
-}
-
-#divcontent{
-  position: absolute;
-  background-color: #4CFFDBaa;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-  width: 100%;
-  height: 0px;
-  transition-property: height, display;
-  transition-duration: 0.3s;
-  -webkit-transition:  height 0.3s;
-  transition-timing-function: ease;
-}
-
-.lista li{
-    display:block;
-    visibility: hidden;
-    height:0px;
-    transition-property: height, visibility;
-    transition-duration: 0.3s;
-    transition-delay:1s;
-    -webkit-transition:  height 1s;
-  transition-timing-function: ease; 
-}
-
-
 .limpafloat{
     clear:both;
 }
@@ -206,42 +105,8 @@ body {
 padding: 0 0;
 margin: 0 0;
 }
-.dropdown:hover #divcontent{
-    height: 120px;
-}
-.dropdown:hover .lista li{
-   visibility:visible;
-    height:35px;
-   transition-property: height, display;
-  transition-duration: 0.3s;
-  -webkit-transition:  height 0.3s;
-  transition-timing-function: ease;
-}
-
-.textos2{
-    font-family: tahoma;
-}
-
-
 
 header{
     width: 100%;
 }
-#divcontent2{
-  position: absolute;
-  background-color: #4CFFDBaa;
-  min-width: 140px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-  width: 100%;
-  height: 0px;
-  transition-property: height, display;
-  transition-duration: 0.3s;
-  -webkit-transition:  height 0.3s;
-  transition-timing-function: ease;
-}
-.dropdown:hover #divcontent2{
-    height: 70px;
-}
-
       </style>
