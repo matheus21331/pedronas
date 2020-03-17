19a098d3647c53d277d48fc88323219303ff46e0
Tiqueliro <matteus.camargo@gmail.com>
2020-03-17 14:11:52 -0300

    ba irmao fiz o single materiais naquele jeitao daquenaip

---
+++ wp-content/themes/pedra-vidros/single-materiais.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+<main class="materiais">
+    <div class="container">
+        <div class="content">
+            <?php $query = get_queried_object();
+                echo $query->post_content
+            ?>
+        </div>
+    <?php if( have_rows('galeria_materiais') ): ?>
+        <div class="row">
+            <?php 
+                while( have_rows('galeria_materiais') ): the_row(); 
+                $imgMateriais = get_sub_field('imagem_materiais_inside');
+            ?>
+                <div class="col-md-2">
+                    <div class="block-img">
+                        <a class="popup-img" href="<?php echo $imgMateriais['url']?>">
+                            <div class="list-img" style="background-image: url( '<?php echo $imgMateriais['url']?>' )"></div>
+                            <h3><?php the_sub_field('titulo_materiais_inside'); ?></h3>
+                        </a>
+                    </div>
+                </div>
+            <?php endwhile;?>
+        </div>
+    <?php endif;?>    
+    </div>
+</main>
+   
+   
+<?php get_footer(); ?>
