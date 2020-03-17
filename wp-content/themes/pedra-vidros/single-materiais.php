891cf555211df4fa391af3f1982e3de59f7315f3
Matheus <matheusbernardi@santins.com.br>
2020-03-17 11:51:05 -0300

    ta quase acabando pelo amor de dues

--- wp-content/themes/pedra-vidros/single-materiais.php
+++ wp-content/themes/pedra-vidros/single-materiais.php
@@ -6,17 +6,17 @@
                 echo $query->post_content
             ?>
         </div>
-    <?php if( have_rows('galeria_ambientes') ): ?>
+    <?php if( have_rows('galeria_materiais') ): ?>
         <div class="row">
             <?php 
-                while( have_rows('galeria_ambientes') ): the_row(); 
-                $imgAmbientes = get_sub_field('imagem_ambientes_inside');
+                while( have_rows('galeria_materiais') ): the_row(); 
+                $imgmateriais = get_sub_field('imagem_materiais_inside');
             ?>
                 <div class="col-md-4">
                     <div class="block-img">
-                        <a class="popup-img" href="<?php echo $imgAmbientes['url']?>">
-                            <div class="list-img" style="background-image: url( '<?php echo $imgAmbientes['url']?>' )"></div>
-                            <h3 class="title-inseide"><?php the_sub_field('titulo_ambientes_inside'); ?></h3>
+                        <a class="popup-img" href="<?php echo $imgMateriais['url']?>">
+                            <div class="list-img" style="background-image: url( '<?php echo $imgMateriais['url']?>' )"></div>
+                            <h3 class="title-inseide"><?php the_sub_field('titulo_materiais_inside'); ?></h3>
                         </a>
                     </div>
                 </div>
